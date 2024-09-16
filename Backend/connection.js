f397e5bc04d1108564b04e7d536d54f3e6abacfd
vishalsuriya <vishalsuriya2003@gmail.com>
2024-09-16 17:18:50 +0530

    changes

--- Backend/connection.js
+++ Backend/connection.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const {Client} = require("pg");
 const DB = new Client({
     connectionString :process.env.DATABASE_URL
+    
 })
 DB.connect((err)=>{
     if(err){
--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const DB = require("./connection");
 const dotenv = require("dotenv");
-const PORT  = 8000;
+const PORT  = process.env.PORT|| 5000;
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
